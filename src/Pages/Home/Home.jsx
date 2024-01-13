c1addb5b30d5662aa807bbff4e517ac1d547e5a7
Shahidul-Islam-Jony <shahidulislam13sf@gmail.com>
2024-01-13 23:21:41 +0600

    error page merge complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,8 +5,8 @@ import TeamMembers from "../TeamMembers/TeamMembers";
 const Home = () => {
     return (
         <div>
-            <Footer></Footer>
             <TeamMembers></TeamMembers>
+            <Footer></Footer>
         </div>
     );
 };
