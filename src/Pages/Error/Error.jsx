883a558e8a4e50b50f09b565ea115d1f5f438bcb
md abu bokor siddik <mdabubokorsiddikbissness@gmail.com>
2024-01-13 20:31:48 +0600

    added error page

--- src/Pages/Error/Error.jsx
+++ src/Pages/Error/Error.jsx
@@ -1,9 +1,17 @@
-
-
+import Lottie from "lottie-react";
+import animations from '/public/Animation - 1705064527975.json'
+import { Link } from "react-router-dom";
 const Error = () => {
   return (
-    <div>
-      <h1>error page </h1>
+    <div className="max-w-full min-h-[800px]  ">
+        {/* error page  */}
+        <div className="my-20" ><h1 className="text-4xl text-center my-4 italic">Error Page Not Found</h1>
+        {/* lottie file */}
+      <Lottie  animationData={animations} className="h-[600px]"></Lottie>
+      <Link to='/'><button className="flex mx-auto btn btn-outline btn-error  italic">Go Home</button></Link>
+      </div>
+      
+      
     </div>
   )
 }
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,11 +1,13 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout";
 import Home from "../Pages/Home/Home";
+import Error from "../Pages/Error/Error";
 
 const routes= createBrowserRouter([
     {
         path:'/',
         element:<MainLayout></MainLayout>,
+        errorElement:<Error></Error>,
         children:[
             {
                 path:'/',
